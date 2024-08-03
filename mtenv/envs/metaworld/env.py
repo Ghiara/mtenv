a06353b79cb9d43a8ea13f70c4b7dbb9dde91cc5
Yuan Meng <yuanmeng961228@gmail.com>
2024-08-03 18:24:10 +0200

    env modified

--- mtenv/envs/metaworld/env.py
+++ mtenv/envs/metaworld/env.py
@@ -147,6 +147,127 @@ def get_list_of_func_to_make_envs(
     return funcs_to_make_envs, env_id_to_task_map
 
 
+####################################################
+# Modified by Y.Meng, return a list of created env #
+####################################################
+def get_list_of_envs(
+    benchmark: Optional[metaworld.Benchmark],
+    benchmark_name: str,
+    env_id_to_task_map: Optional[EnvIdToTaskMapType],
+    should_perform_reward_normalization: bool = True,
+    task_name: str = "pick-place-v1",
+    num_copies_per_env: int = 1,
+) -> Tuple[List[Any], Dict[str, Any]]:
+    """Return a list of functions to construct the MetaWorld environments
+    and a mapping of environment ids to tasks.
+
+    Args:
+        benchmark (Optional[metaworld.Benchmark]): `benchmark` to create
+            tasks from.
+        benchmark_name (str): name of the `benchmark`. This is used only
+            when the `benchmark` is None.
+        env_id_to_task_map (Optional[EnvIdToTaskMapType]): In MetaWorld,
+            each environment can be associated with multiple tasks. This
+            dict persists the mapping between environment ids and tasks.
+        should_perform_reward_normalization (bool, optional): Defaults to
+            True.
+        task_name (str, optional): In case of MT1, only . Defaults to
+            "pick-place-v1".
+        num_copies_per_env (int, optional): Number of copies to create for
+            each environment. Defaults to 1.
+
+    Raises:
+        ValueError: if `benchmark` is None and `benchmark_name` is not
+            MT1, MT10, or MT50.
+
+    Returns:
+        Tuple[List[Any], Dict[str, Any]]: A tuple of two elements. The
+        first element is a list of functions to construct the MetaWorld
+        environments and the second is a mapping of environment ids
+        to tasks.
+
+    """
+    if not benchmark:
+        if benchmark_name == "MT1":
+            benchmark = metaworld.ML1(task_name)
+        elif benchmark_name == "MT10":
+            benchmark = metaworld.MT10()
+        elif benchmark_name == "MT50":
+            benchmark = metaworld.MT50()
+        else:
+            raise ValueError(f"benchmark_name={benchmark_name} is not valid.")
+
+    env_id_list = list(benchmark.train_classes.keys())
+
+    def _get_class_items(current_benchmark):
+        return current_benchmark.train_classes.items()
+
+    def _get_tasks(current_benchmark):
+        return current_benchmark.train_tasks
+
+    def _get_env_id_to_task_map() -> EnvIdToTaskMapType:
+        env_id_to_task_map: EnvIdToTaskMapType = {}
+        current_benchmark = benchmark
+        for env_id in env_id_list:
+            for name, _ in _get_class_items(current_benchmark):
+                if name == env_id:
+                    task = random.choice(
+                        [
+                            task
+                            for task in _get_tasks(current_benchmark)
+                            if task.env_name == name
+                        ]
+                    )
+                    env_id_to_task_map[env_id] = task
+        return env_id_to_task_map
+
+    if env_id_to_task_map is None:
+        env_id_to_task_map: EnvIdToTaskMapType = _get_env_id_to_task_map()  # type: ignore[no-redef]
+    assert env_id_to_task_map is not None
+
+    def make_envs_use_id(env_id: str):
+        current_benchmark = benchmark
+        
+        
+        def _make_env():
+            for name, env_cls in _get_class_items(current_benchmark):
+                if name == env_id:
+                    env = env_cls()
+                    task = env_id_to_task_map[env_id]
+                    env.set_task(task)
+                    if should_perform_reward_normalization:
+                        env = NormalizedEnvWrapper(env, normalize_reward=True)
+                    return env
+        # modified return built single envs
+        single_env = _make_env()
+        return single_env
+
+    if num_copies_per_env > 1:
+        env_id_list = [
+            [env_id for _ in range(num_copies_per_env)] for env_id in env_id_list
+        ]
+        env_id_list = [
+            env_id for env_id_sublist in env_id_list for env_id in env_id_sublist
+        ]
+
+    list_of_envs = [make_envs_use_id(env_id) for env_id in env_id_list]
+    # return a list funcs to create envs, mapping between id and tasks
+    '''
+    For MT10:
+    [<NormalizedEnvWrapper<SawyerReachPushPickPlaceEnv instance>>,
+    <NormalizedEnvWrapper<SawyerReachPushPickPlaceEnv instance>>,
+    <NormalizedEnvWrapper<SawyerReachPushPickPlaceEnv instance>>,
+    <NormalizedEnvWrapper<SawyerDoorEnv instance>>,
+    <NormalizedEnvWrapper<SawyerDrawerOpenEnv instance>>,
+    <NormalizedEnvWrapper<SawyerDrawerCloseEnv instance>>,
+    <NormalizedEnvWrapper<SawyerButtonPressTopdownEnv instance>>,
+    <NormalizedEnvWrapper<SawyerPegInsertionSideEnv instance>>,
+    <NormalizedEnvWrapper<SawyerWindowOpenEnv instance>>,
+    <NormalizedEnvWrapper<SawyerWindowCloseEnv instance>>]
+    '''
+    return list_of_envs, env_id_to_task_map
+
+
 def build(
     benchmark: Optional[metaworld.Benchmark],
     benchmark_name: str,
